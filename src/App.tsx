5864ca5641964525facb0071c921b6f34ddbb9c1
Sergey_Petrachev <mastertso2241@gmail.com>
2023-12-04 13:38:33 +0300

    add material-ui lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
+import { Button } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       hello react
+
     </div>
   );
 }
